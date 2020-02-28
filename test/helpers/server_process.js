6418ff43707d6c448ee6f51f1e4e33c6e62badf3
Yoseph Maguire <yoseph.maguire@microsoft.com>
2020-02-28 11:00:41 -0800

    fixes

--- test/helpers/server_process.js
+++ test/helpers/server_process.js
@@ -2,11 +2,8 @@
 
 var MqttServer = require('../server').MqttServer
 
-console.log('here i am')
 new MqttServer(function (client) {
-  console.log('creating mqttserver')
   client.on('connect', function () {
-    console.log('connack received')
     client.connack({ returnCode: 0 })
   })
 }).listen(3000, 'localhost')
